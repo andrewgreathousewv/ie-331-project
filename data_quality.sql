010c6f868a6fdbf1396fefb5a7cdd7ae6c687afe
Andrew Greathouse <abg00014@mix.wvu.edu>
2026-03-24 15:21:57 -0400

    NULL rates for key columns: customer_id, order_id, product_id, and seller_id. Worked with another classmate.

--- data_quality.sql
+++ data_quality.sql
@@ -25,8 +25,7 @@ SELECT * FROM row_counts;
 # NULL rates for key columnns
 # Check for missing values in key columns
 # Use NULL to find data that was not recorded or missing, count how many times it occured.
-# The group went forward with using key columns- order id,customer, id, and order status
-# We attempted to put product id instead of order status but had to stay with selecting from "orders"
+# The group went forward with using key columns: order_id,customer_id, product_id, and seller_id
 # Columns headers are considered our key columns
 WITH null_checks AS (
     select
@@ -34,32 +33,40 @@ WITH null_checks AS (
         COUNT(*) AS total_rows,
         SUM(CASE WHEN order_id IS NULL THEN 1 ELSE 0 END) as  null_order_id,
         SUM(CASE WHEN customer_id IS NULL THEN 1 ELSE 0 END) as  null_customer_id,
-        SUM(CASE WHEN order_status IS NULL THEN 1 ELSE 0 END) as null_order_status
+        0 as null_product_id,
+        0 as nul_seller_id
     from orders
   UNION ALL
     select
         'customers',
         COUNT(*),
         SUM(CASE WHEN customer_id IS NULL THEN 1 ELSE 0 END),
-        SUM(CASE WHEN customer_unique_id IS NULL THEN 1 ELSE 0 END),
-        SUM(CASE WHEN customer_zip_code_prefix IS NULL THEN 1 ELSE 0 END)
+        0 as null_order_id,
+        0 as null_product_id,
+        0 as null_seller_id
     from customers
     UNION ALL
     select
         'products',
         COUNT(*),
         SUM(CASE WHEN product_id IS NULL THEN 1 ELSE 0 END),
-        SUM(CASE WHEN product_category_name IS NULL THEN 1 ELSE 0 END),
-        SUM(CASE WHEN product_weight_g IS NULL THEN 1 ELSE 0 END)
+        0 as null_order_id,
+        0 as null_customer_id,
+        0 as seller_id
     from products
     UNION ALL
     select
       'sellers',
       COUNT(*),
        SUM(CASE WHEN seller_id IS NULL THEN 1 ELSE 0 END),
-        SUM(CASE WHEN seller_city IS NULL THEN 1 ELSE 0 END),
-        SUM(CASE WHEN seller_state IS NULL THEN 1 ELSE 0 END)
+       0 as nuller_order_id,
+       0 as null_customer_id,
+       0 as null_product_id
         from sellers
 )
 SELECT * FROM null_checks;
 #test
+# Orphaned foreign keys
+# id links tables (Ex. customer_id)
+# A row that is orphaned means one table references an id that has no match in another table
+#
