1aa5c197610c2f31474277664379b40bc87e7259
Andrew Greathouse <abg00014@mix.wvu.edu>
2026-03-24 11:43:18 -0400

    Documented row counts per table

--- data_quality.sql
+++ data_quality.sql
@@ -1,1 +1,23 @@
 #Data quality audit file
+# Using Olist data set
+# Row counts per table
+WITH row_counts AS (
+  SELECT 'category_translation', COUNT(*) FROM category_translation
+    UNION ALL
+    SELECT 'customers', COUNT(*) FROM customers
+    UNION ALL
+    SELECT 'geolocation', COUNT(*) FROM geolocation
+    UNION ALL
+    SELECT 'order_items', COUNT(*) FROM order_items
+    UNION ALL
+    SELECT 'order_payments', COUNT(*) FROM order_payments
+    UNION ALL
+    SELECT 'order_reviews', COUNT(*) FROM order_reviews
+    UNION ALL
+    SELECT 'orders' AS table_name, COUNT(*) AS row_count FROM orders
+    UNION ALL
+    SELECT 'products', COUNT(*) FROM products
+    UNION ALL
+    SELECT 'sellers', COUNT(*) FROM sellers
+    )
+SELECT * FROM row_counts;
