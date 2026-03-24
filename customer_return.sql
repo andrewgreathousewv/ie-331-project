53db811b828cd8c583bec86a3f004fb630af3a62
BradyStafford54 <bas00054@mix.wvu.edu>
2026-03-24 14:44:48 -0400

    Update customer_return.sql

--- customer_return.sql
+++ customer_return.sql
@@ -1,10 +1,32 @@
 #Question: What percentage of customers return within 30, 60, and 90 days of their first purchase?
 
 #1 Identify Purchase
+# When did each customer buy their first item
 WITH first_order AS (
     SELECT 
         customer_id,
         MIN(order_purchase_timestamp) AS first_purchase
     FROM orders
     GROUP BY customer_id
+),
+
+#2 Combine future orders with customers
+# For every order, we will now know when customer started
+all_orders AS (
+    SELECT 
+        o.customer_id,
+        o.order_purchase_timestamp,
+        f.first_purchase
+    FROM orders o
+    JOIN first_order f 
+        ON o.customer_id = f.customer_id
+),
+
+#3 Calculate time differences
+# How long was it until the customer returned
+time_diffs AS (
+    SELECT 
+        customer_id,
+        DATE_DIFF('day', first_purchase, order_purchase_timestamp) AS days_since_first
+    FROM all_orders
 ),
